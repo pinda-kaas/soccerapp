f9bde7aafbcffffabc9b034b66ee25440ea5f6aa
kneeinjury <dingen2010@gmail.com>
2015-05-01 16:45:57 +1000

    first version

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name soccerAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the soccerAppApp
+ */
+angular.module('soccerAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
